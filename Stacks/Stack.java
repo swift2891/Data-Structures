2a1ca458378541a25df389691fa817c5f0ff3e51
Vigneshwar Padmanaban <swift2891@gmail.com>
2017-11-03 23:11:06 -0400

    Stacks DS

---
+++ Stacks/Stack.java
@@ -0,0 +1,35 @@
+public Class Stack{
+
+	private int pointer;
+	public Stack(int arraySize){
+		int[] stackArray = new int[arraySize];
+		pointer = -1;
+	}
+	public Stack(){
+		//default size
+		int[] stackArray = new int[100];	
+		pointer = -1;
+	}
+
+	public void push(int newItem){
+		pointer += 1;
+		stackArray[pointer] = newItem;
+	}
+
+	public int pop(){
+		int currentItem = stackArray[pointer];
+		pointer -= 1;
+		return currentItem;
+	}
+
+	public static void main(String args[]){
+
+		Stack s = new Stack(10);
+		s.push(1);
+		s.push(2);
+		s.push(3);
+		System.out.println(s.pop());
+		System.out.println(s.pop());
+		System.out.println(s.pop());
+	}
+}
